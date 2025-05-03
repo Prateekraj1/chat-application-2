1619778c1751d201856e2043d32dd5d175b8565c
Prateek Raj <67007560+Prateekraj1@users.noreply.github.com>
2025-05-03 19:21:47 +0530

    changes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios.js';
 import { io } from 'socket.io-client';
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 export const useAuthStore = create((set, get) => ({
     authUser: null,
     isLogingIn: false,
